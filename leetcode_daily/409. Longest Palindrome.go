746ba1f36377461e49790efdd6a6ea6c21e8e181
naslth <letrunghieu10x@gmail.com>
2024-06-04 22:19:59 +0700

    4-6-24

---
+++ leetcode_daily/409. Longest Palindrome.go
@@ -0,0 +1,23 @@
+func longestPalindrome(s string) int {
+	res := 0
+	dict := make(map[string]int)
+	for _, v := range s {
+		if _, ok := dict[string(v)]; ok {
+			dict[string(v)]++
+		} else {
+			dict[string(v)] = 1
+		}
+		value := dict[string(v)]
+		if value == 2 {
+			dict[string(v)] = 0
+			res += 2
+		}
+	}
+	for _, value := range dict {
+		if value == 1 {
+			res++
+			break
+		}
+	}
+	return res
+}
