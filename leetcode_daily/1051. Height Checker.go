a592819a110518b2a58922bcd0d13b9a4546aed3
naslth <letrunghieu10x@gmail.com>
2024-06-11 21:34:11 +0700

    11-6-24

---
+++ leetcode_daily/1051. Height Checker.go
@@ -0,0 +1,13 @@
+func heightChecker(heights []int) int {
+    expected := make([]int, len(heights))
+    copy(expected, heights)
+    sort.Ints(expected)
+    res := 0
+    for i := 0;i<len(expected);i++ {
+        fmt.Print(expected[i],heights[i])
+        if expected[i] != heights[i] {
+            res++
+        }
+    }
+    return res
+}
---
+++ leetcode_daily/1122. Relative Sort Array.go
@@ -0,0 +1,26 @@
+func relativeSortArray(arr1 []int, arr2 []int) []int {
+	res := []int{}
+	map1 := make(map[int]int)
+	res2 := []int{}
+	for _, num1 := range arr1 {
+		map1[num1]++
+
+	}
+	for _, num2 := range arr2 {
+		for map1[num2] > 0 {
+			res = append(res, num2)
+			map1[num2]--
+		}
+	}
+	for key, value := range map1 {
+		if value > 0 {
+			for value > 0 {
+				res2 = append(res2, key)
+				value--
+			}
+		}
+	}
+	sort.Ints(res2)
+	res3 := append(res, res2...)
+	return res3
+}
