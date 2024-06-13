97e6f1d4bc40cdf1efc90e62b891bb5b4f776f9d
naslth <letrunghieu10x@gmail.com>
2024-06-13 22:03:34 +0700

    13-6-24

---
+++ leetcode_daily/2037. Minimum Number of Moves to Seat Everyone.go
@@ -0,0 +1,9 @@
+func minMovesToSeat(seats []int, students []int) int {
+	sort.Ints(seats)
+	sort.Ints(students)
+	res := 0
+	for i := 0; i < len(seats); i++ {
+		res +=  int(math.Abs(float64(seats[i]) - float64(students[i])))
+	}
+	return res
+}
