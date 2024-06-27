c1af367696990fdbc67f4f8e4d4804e83c769c5c
naslth <letrunghieu10x@gmail.com>
2024-06-27 21:29:03 +0700

    27-6-24

---
+++ leetcode_daily/1791. Find Center of Star Graph.go
@@ -0,0 +1,14 @@
+func findCenter(edges [][]int) int {
+    temp := make(map[int]int)
+    for _, edge := range edges {
+        temp[edge[0]]++;
+        temp[edge[1]]++;
+        if temp[edge[0]] > 1  {
+            return edge[0]
+        }
+        if  temp[edge[1]] > 1 {
+            return edge[1]
+        }
+    }
+    return 0
+}
