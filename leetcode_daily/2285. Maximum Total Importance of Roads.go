886fc712dbc282425a94ea7de07f342270f02223
naslth <letrunghieu10x@gmail.com>
2024-06-28 20:41:09 +0700

    28-6-24

---
+++ leetcode_daily/2285. Maximum Total Importance of Roads.go
@@ -0,0 +1,26 @@
+type kv struct {
+	Key   int
+	Value int
+}
+
+func maximumImportance(n int, roads [][]int) int64 {
+	temp := make(map[int]int)
+	for _, road := range roads {
+		temp[road[0]]++
+		temp[road[1]]++
+	}
+	var kvSlice []kv
+	for k, v := range temp {
+		kvSlice = append(kvSlice, kv{k, v})
+	}
+	sort.Slice(kvSlice, func(i, j int) bool {
+		return kvSlice[i].Value > kvSlice[j].Value
+	})
+	i := n
+	var res int64
+	for _, kv := range kvSlice {
+		res += int64(kv.Value * i)
+		i--
+	}
+	return res
+}
